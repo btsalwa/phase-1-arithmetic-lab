808b03365cfe58716ab36df377adb48096d93d20
btsalwa <brian.tsalwa@student.moringaschool.com>
2023-09-27 18:05:52 +0300

    arithmetic done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1) * (num2) 
+
+let random = Math.floor(Math.random() *10)
+random > 0;
+
+let num3 = 8;
+let num4 = 2;
+let mod = (num3) / (num4) ;
+
+let number1 = 10;
+let number2 = 20;
+let max = Math.max(number1, number2);
+
